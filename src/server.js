dddd240b0450b454c39860b7d9651e80fda3969d
Jyoti-coder1 <jyotimathsdev@gmail.com>
2025-11-04 21:09:12 +0530

    Day 1 complete

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('TalentPool backend is running'));
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+const protectedRoutes = require('./routes/protected');
+app.use('/api/protected', protectedRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
